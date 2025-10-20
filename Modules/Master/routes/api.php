4ecff055d08f850948bae3f760e083c90229a161
ElyaNur <sitinurjanah_1601617043@mhs.unj.ac.id>
2025-10-20 11:04:01 +0700

    remove some module

--- Modules/Master/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Master\Http\Controllers\MasterController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('masters', MasterController::class)->names('master');
-});
